a53f1d28dee3e6345991ba8573327f06e5d72eeb
Luiz <felipeluizsantos.k@gmail.com>
2022-11-25 13:28:15 -0300

    Testando commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu("./database/db.txt"); // No lugar do CaminhoDoArquivo, deve ser algo como "./database.txt"
         menu.interact();
+        //COMENTARIO NOVO PRA TESTE
     }
 }
