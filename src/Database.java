5ee5e166f0027738fb8bba331a1bd752c005b6e9
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2022-11-25 15:33:05 -0300

    implementa persist, buy, e buyProduct

--- src/Database.java
+++ src/Database.java
@@ -1,51 +1,66 @@
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 
 public class Database {
     private List<Product> products;
+    private String filePath;
 
-    Database(List<Product> file) {
-        this.products = file;
+    Database(List<Product> products, String filePath) {
+        this.products = products;
+        this.filePath = filePath;
     }
 
     public List<Product> getProducts() {
         return products;
     }
 
+    private void persist() {
+        try {
+            FileWriter myWriter = new FileWriter(filePath, false);
+            for (Product product : products) {
+                myWriter.append(product.getName() + ";" + product.getQuantity() + ";" + product.getPrice() + "\n");
+            }
+            myWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void create(Product product) {
         products.add(product);
+        persist();
     }
 
     public void edit(Product product, Integer index) throws IndexOutOfBoundsException{
         //Testar o erro
         products.set(index, product);
+        persist();
     };
 
     public void delete(Integer index) throws IndexOutOfBoundsException{
         //Testar o erro
-        products.remove(index);
+        products.remove((int)index);
+        persist();
     };
 
     public void search(String substring) {
         for (int i = 0; i < products.size(); i++) {
             if (products.get(i).getName().contains(substring)){
-                // Formatar bonitinho
                 System.out.println("ID: " + i + "; " + products.get(i).toString());
             }
         }
     };
 
     public void buy(Integer index, Integer orderQuantity) {
-        products.get(index).reduceStock(orderQuantity);
-    };
-
-    public void close(){
-        // Desnecessário? Talvez só o close no scanner na classe menu
+        Product product = products.get(index);
+        product.reduceStock(orderQuantity);
+        products.set(index, product);
+        persist();
     };
 }
-/*
-Venda de produtos, onde o usuário pode escolher produtos e quantidades conforme ele queira, assim que escolher finalizar,
-mostre tudo que ele comprou, os preços e o total. Quando ele for escolher o produto e quantidade, faça uma verificação
-se o produto tem aquela quantidade, caso não tenha, informa ao usuário que não contém a quantidade deste produto no estoque.
-Assim que o usuário confirmar a compra, deduza as quantidades dos produtos selecionados.
- */
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Menu menu = new Menu("./database/db.txt"); // No lugar do CaminhoDoArquivo, deve ser algo como "./database.txt"
+        Menu menu = new Menu("./database/db.txt");
         menu.interact();
     }
 }
